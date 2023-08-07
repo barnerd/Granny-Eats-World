fa9224d4ac091b78f9a45c9c9f11840c04a3ae97
David Barner <dibarner@gmail.com>
2023-08-07 08:18:18 -0700

    PauseMenu cleanup

--- Assets/Scripts/SceneManagement/PauseMenu.cs
+++ Assets/Scripts/SceneManagement/PauseMenu.cs
@@ -1,33 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-
     public GameObject PausePanel;
-    
+
     public static bool GameIsPaused = false;
 
     // Update is called once per frame
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.Tab)) {
-            if (GameIsPaused) {
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (GameIsPaused)
+            {
                 Resume();
             }
-            else {
+            else
+            {
                 Pause();
             }
         }
     }
-    
-    void Pause() {
+
+    void Pause()
+    {
         PausePanel.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
-    
-    void Resume() {
+
+    void Resume()
+    {
         PausePanel.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
