29b1d4fcecfa5c88f4d33f09de77ba9d796b43fb
David Barner <dibarner@gmail.com>
2023-08-13 13:10:02 -0700

    Moved Check/Craft Recipe to RecipeData

--- Assets/Scripts/Crafting/CraftingSystem.cs
+++ Assets/Scripts/Crafting/CraftingSystem.cs
@@ -7,43 +7,6 @@ public class CraftingSystem : MonoBehaviour
     public List<RecipeData> recipes;
 
     /// <summary>
-    /// Check if list of items can craft given recipe
-    /// </summary>
-    /// <param name="_inputItems">list of items to be used in crafting</param>
-    /// <param name="_recipe">the recipe to craft</param>
-    /// <returns>true if the given recipe can be crafted, otherwise false</returns>
-    public bool CheckRecipe(List<ItemData> _inputItems, RecipeData _recipe)
-    {
-        if (_inputItems != null && _recipe != null)
-        {
-            foreach (var _item in _recipe.inputItems)
-            {
-                if (!_inputItems.Contains(_item))
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    /// <summary>
-    /// Craft the given recipe from the provided items
-    /// </summary>
-    /// <param name="_inputItems">list of items to be used in crafting</param>
-    /// <param name="_recipe">the recipe to craft</param>
-    /// <returns>resulting items if recipe can be crafted, otherwise null</returns>
-    public List<ItemData> CraftRecipe(List<ItemData> _inputItems, RecipeData _recipe)
-    {
-        return CheckRecipe(_inputItems, _recipe) && _recipe != null ? _recipe.outputItems : null;
-    }
-
-    /// <summary>
     /// Checks all known recipes to see if one can be crafted
     /// </summary>
     /// <param name="_inputItems">list of items to be used in crafting</param>
@@ -54,7 +17,7 @@ public class CraftingSystem : MonoBehaviour
 
         foreach (var _recipe in recipes)
         {
-            _result |= CheckRecipe(_inputItems, _recipe);
+            _result |= _recipe.CheckRecipe(_inputItems);
         }
 
         return _result;
--- Assets/Scripts/Crafting/RecipeData.cs
+++ Assets/Scripts/Crafting/RecipeData.cs
@@ -7,4 +7,41 @@ public class RecipeData : ScriptableObject
 {
     public List<ItemData> inputItems;
     public List<ItemData> outputItems;
+
+    /// <summary>
+    /// Check if list of items can craft given recipe
+    /// </summary>
+    /// <param name="_inputItems">list of items to be used in crafting</param>
+    /// <param name="_recipe">the recipe to craft</param>
+    /// <returns>true if the given recipe can be crafted, otherwise false</returns>
+    public bool CheckRecipe(List<ItemData> _inputItems)
+    {
+        if (_inputItems == null)
+        {
+            return false;
+        }
+        else
+        {
+            foreach (var _item in inputItems)
+            {
+                if (!_inputItems.Contains(_item))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Craft the given recipe from the provided items
+    /// </summary>
+    /// <param name="_inputItems">list of items to be used in crafting</param>
+    /// <param name="_recipe">the recipe to craft</param>
+    /// <returns>resulting items if recipe can be crafted, otherwise null</returns>
+    public List<ItemData> CraftRecipe(List<ItemData> _inputItems)
+    {
+        return CheckRecipe(_inputItems) ? outputItems : null;
+    }
 }
