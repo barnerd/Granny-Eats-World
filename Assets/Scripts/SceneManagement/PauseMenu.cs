b21572182083502a90fa9331d36e3e04f9be94ab
WilsamGlitch <sam.elaine.wilson@gmail.com>
2023-08-25 14:37:26 -0700

    Update PauseMenu.cs
    added functions to allow the pause menu to be disabled and enabled

--- Assets/Scripts/SceneManagement/PauseMenu.cs
+++ Assets/Scripts/SceneManagement/PauseMenu.cs
@@ -5,11 +5,12 @@ public class PauseMenu : MonoBehaviour
     public GameObject PausePanel;
 
     public static bool GameIsPaused = false;
+    private bool canPause = true;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab))
+        if (Input.GetKeyDown(KeyCode.Tab) && canPause)
         {
             if (GameIsPaused)
             {
@@ -34,5 +35,13 @@ public class PauseMenu : MonoBehaviour
         PausePanel.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+    }
+    
+    public void CantPause() {
+        canPause = false;
+    }
+    
+    public void CanPause() {
+        canPause = true;
     }
 }
