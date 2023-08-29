42f33dee775c76a67bf2f7541fe58b22decd961e
David Barner <dibarner@gmail.com>
2023-08-29 14:08:36 -0700

    Witch's dialogue

--- Assets/Scripts/Magic Shop Scripts/WitchDialog.cs
+++ Assets/Scripts/Magic Shop Scripts/WitchDialog.cs
@@ -7,33 +7,51 @@ public class WitchDialog : MonoBehaviour
 {
     public GameObject dialogBox;
     public MagicShop buttons;
-    public Button witch;
+    public Image speakerImage;
     public Text message;
+    public Sprite[] speakers; // HACK: this should be an object instead of hoping the indexes match
+    public string[] messages;
+    private int currentIndex;
     private bool showing = false;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         dialogBox.SetActive(false);
+        currentIndex = -1;
     }
-    
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.E) && showing == true) {
-            HideDialog();
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && showing == true)
+        {
+            ShowNextDialogue();
         }
     }
-    
-    public void HideDialog() {
+
+    public void HideDialogue()
+    {
         dialogBox.SetActive(false);
         showing = false;
         buttons.EnableButtons();
+        currentIndex = -1;
     }
 
-    public void ShowDialog() {
-        dialogBox.SetActive(true);
-        showing = true;
-        buttons.DisableButtons();
-        
-        message.text = "Hey, my grandma said you can take what you need to keep your grandma from exploding or whatever";
+    public void ShowNextDialogue()
+    {
+        currentIndex++;
+        if (currentIndex < 0 || currentIndex >= messages.Length)
+        {
+            HideDialogue();
+        }
+        else
+        {
+            dialogBox.SetActive(true);
+            showing = true;
+            buttons.DisableButtons();
+
+            speakerImage.sprite = speakers[currentIndex];
+            message.text = messages[currentIndex]; //"Hey, my grandma said you can take what you need to keep your grandma from exploding or whatever";
+        }
     }
 }
